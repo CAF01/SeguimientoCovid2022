89355bdb42a180dee6da283782360e64ce79cf3d
Christian Alcantara <Christian_pria@hotmail.com>
2022-07-25 13:21:49 -0500

    Entidades para el manejo de Operaciones para Profesores
    - FiltroProgramaPeriodo
    - FiltroSeguimientoProfesor
    - Incapacidad
    - PositivoProfe
    - ProfeGrupo
    - Profesor
    - SeguimientoProfesor
    - LogicaNegociosProfesor
    --- Adición del paquete para Bootstrap

---
+++ SolutionCovid/ClassEntidades/Incapacidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntidades
+{
+    public class Incapacidad
+    {
+        public int id_Incapacidad { get; set; }
+        public DateTime Fecha_otorga { get; set; }
+        public DateTime Fecha_finalizacion { get; set; }
+        public string IncapacidadUrl { get; set; }
+        public int id_posProfe { get; set; }
+    }
+}

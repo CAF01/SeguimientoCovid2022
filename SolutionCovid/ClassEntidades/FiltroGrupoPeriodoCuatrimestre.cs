89f73c02ccc73bfd4075bf21769aa157f5083555
Christian Alcantara <Christian_pria@hotmail.com>
2022-07-30 13:14:05 -0500

    Integración del modulo para manejo Grupos Profesor
    - Se modifico la lógica de negocios para Profesor.
    - Se agrego el WebForm WebGrupoProfe

---
+++ SolutionCovid/ClassEntidades/FiltroGrupoPeriodoCuatrimestre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntidades
+{
+    public class FiltroGrupoPeriodoCuatrimestre
+    {
+        public int id_GruCuat { get; set; }
+        public string Grupo { get; set; }
+        public string Turno { get; set; }
+        public string Modalidad { get; set; }
+        public string Periodo { get; set; }
+        public int Anio { get; set; }
+    }
+}
